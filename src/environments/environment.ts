97887c7bb3af58b698717c3983cb8195d85d93ca
victor-souza-dev <victor.souza2210@gmail.com>
2026-03-01 23:00:19 -0300

    fix(environment): ensure apiUrl defaults to '/api' when NG_APP_API_URL is not set

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: import.meta.env.NG_APP_PRODUCTION === 'true',
-  apiUrl: `${import.meta.env.NG_APP_API_URL}/api` || '/api',
+  apiUrl: import.meta.env.NG_APP_API_URL ? `${import.meta.env.NG_APP_API_URL}/api` : '/api',
 };
